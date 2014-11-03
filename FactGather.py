48616fbbf132560dff722fca285c015829f28333
Matt Oswalt <matt@keepingitclassless.net>
2014-11-03 14:07:37 -0800

    Added example script for NXAPI Connection
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

---
+++ FactGather.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+"""
+Modify these please
+"""
+url='http://192.168.1.123/ins'
+switchuser='admin'
+switchpassword='Cisco.com'
+
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_show",
+    "chunk": "0",
+    "sid": "1",
+    "input": "show cdp nei",
+    "output_format": "json"
+  }
+}
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+print response
