b415e7d86a836db406ed632c3967bc77c1031787
Greg Clark <gclark@gdt.com>
2014-11-03 17:58:21 -0600

    First version with getters and putters for the three properties

--- week1/device.py
+++ week1/device.py
@@ -1,15 +1,41 @@
 class Device(object):
     # Initializer
-    def __init__(self, name):
-        # An instance variable to hold the student's name
-        self._name = name
+    def __init__(self, ip, user, passw):
+        # An instance variable to hold the device's name
+        self._ip = ip
+        self._user = user
+        self._passw = passw
  
-    # Getter method
+    # IP Getter method
     @property
-    def name(self):
-        return self._name
+    def ip(self):
+        """Get the current ip address."""
+        return self._ip
  
-    # Setter method
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
+    # IP Setter method
+    @ip.setter
+    def ip(self, new_ip):
+        """Change the ip address for this device. """
+        self._ip = new_ip
+        
+    # User Getter method
+    @property
+    def user(self):
+        """Get the current user name for logging on to this device."""
+        return self._user
+ 
+    # User Setter method
+    @user.setter
+    def user(self, new_user):
+        self._user = new_user
+        
+    # Password Getter method
+    @property
+    def passw(self):
+        """Get the current password for logging on to this device."""
+        return self._passw
+ 
+    # Password Setter method
+    @passw.setter
+    def passw(self, new_passw):
+        self._passw = new_passw
